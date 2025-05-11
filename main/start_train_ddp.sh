6b7c0358bfbc15828d9f324d15c9131641f15797
MyLiya <guojie2002@outlook.com>
2025-05-11 14:40:32 +0800

    some train settings, to speed up, dataloader, num_workers, batch_size, cuda_devices

--- main/start_train_ddp.sh
+++ main/start_train_ddp.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=1 torchrun --master_port=29500 --nproc_per_node=1 main/train.py \
+CUDA_VISIBLE_DEVICES=0,1 torchrun --master_port=29500 --nproc_per_node=2 main/train.py \
   --config configs/rtdetrv2/rtdetrv2_r101vd_6x_organoid_linux.yml \
   --tuning checkpoints/rtdetrv2_r101vd_6x_coco_from_paddle.pth \
   --seed 0 \
