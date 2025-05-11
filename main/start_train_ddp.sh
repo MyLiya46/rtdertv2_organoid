4eaeff99baa192cf87da5eb35b0c185023239704
MyLiya <guojie2002@outlook.com>
2025-05-11 12:55:40 +0800

    little bugs of file path

--- main/start_train_ddp.sh
+++ main/start_train_ddp.sh
@@ -1,7 +1,6 @@
-CUDA_VISIBLE_DEVICES=0,1 nohup torchrun --master_port=29500 --nproc_per_node=2 main/train.py \
+CUDA_VISIBLE_DEVICES=0 torchrun --master_port=29500 --nproc_per_node=1 main/train.py \
   --config configs/rtdetrv2/rtdetrv2_r101vd_6x_organoid_linux.yml \
   --tuning checkpoints/rtdetrv2_r101vd_6x_coco_from_paddle.pth \
   --use-amp \
   --seed 0 \
   --output-dir output/exp_rtdetrv2_r101vd_organoid \
-  > log.txt 2>&1 &
