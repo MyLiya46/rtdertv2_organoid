01d64e6894533e45460932c2025d35722b0f5131
MyLiya <guojie2002@outlook.com>
2025-05-11 17:16:53 +0800

    .

--- main/start_train_ddp.sh
+++ main/start_train_ddp.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=1 torchrun --master_port=29500 --nproc_per_node=1 main/train.py \
   --config configs/rtdetrv2/rtdetrv2_r101vd_6x_organoid_linux.yml \
   --tuning checkpoints/rtdetrv2_r101vd_6x_coco_from_paddle.pth \
-  --use_amp \
+  --use-amp \
   --seed 0
