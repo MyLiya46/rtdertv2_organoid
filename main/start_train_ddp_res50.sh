91bae492973f046fa47b9228282a7ad0253b8c05
MyLiya <guojie2002@outlook.com>
2025-05-12 13:03:02 +0800

    res50, freeze at 4

---
+++ main/start_train_ddp_res50.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=1 torchrun --master_port=29500 --nproc_per_node=1 main/train.py \
+  --config configs/rtdetrv2/rtdetrv2_r50vd_organoid_linux.yml \
+  --tuning checkpoints/rtdetrv2_r50vd_6x_coco_ema.pth \
+  --use-amp \
+  --use-ema \
+  --seed 0
