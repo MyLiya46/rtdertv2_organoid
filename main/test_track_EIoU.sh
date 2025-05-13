0f173d17799c53a4f1ba498c6e8495642a02de85
MyLiya <guojie2002@outlook.com>
2025-05-13 13:22:56 +0800

    filename error

--- main/test_track_EIoU.sh
+++ main/test_track_EIoU.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=1 torchrun --master_port=29500 --nproc_per_node=1 main/test_track_EIoU.py \
   --config configs/rtdetrv2/rtdetrv2_r50vd_organoid_linux.yml \
-  --ckpt output/exp_train_rtdetrv2_r50vd_organoid_stomach_cancer_50epoch/best.pth
+  --ckpt output/exp_train_rtdetrv2_r50vd_organoid_stomach_cancer_epoch50/best.pth
