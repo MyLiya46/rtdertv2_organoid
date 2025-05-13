2616743f409365dbcf770a942c379aa63bd46fc3
MyLiya46 <guojie2002@outlook.com>
2025-05-13 14:07:00 +0800

    scripy

--- main/start_train_ddp_res50.sh
+++ main/start_train_ddp_res50.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=1 torchrun --master_port=29500 --nproc_per_node=1 main/train.py \
   --config configs/rtdetrv2/rtdetrv2_r50vd_organoid_linux.yml \
-  --tuning checkpoints/rtdetrv2_r50vd_6x_coco_ema.pth \
+  --resume output/exp_train_rtdetrv2_r50vd_organoid_stomach_cancer_epoch50/best.pth \
   --output-dir output/rtdetrv2_r50vd_organoid \
   --use-amp \
   --seed 0
